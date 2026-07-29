f1d6d08970a5f125027f68970b50011d4047287a
ysthfs <72653077+ysthfs@users.noreply.github.com>
2026-07-30 00:08:48 +0800

    Refactor main.py for enhanced dashboard features
    Updated the dashboard performance script to include new features and improve data handling.

--- main.py
+++ main.py
@@ -36,7 +36,7 @@ st.markdown("""
 </p>
 
 <p style='text-align: center; font-size: 16px; color: #ccc; margin-top: 2px;'>
-    Updates Ticket 19 July 2026
+    Updates Ticket 26 July 2026
 </p>
 <hr style='margin-top: 10px; margin-bottom: 20px;'>
 """, unsafe_allow_html=True)
